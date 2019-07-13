41817303866e1dfe22071580cb6860df2f20f15f
Arnab Chakraborty <arnab033chakraborty@gmail.com>
2019-07-14 00:44:16 +0530

    Initial Commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,22 @@
+@extends('layouts.app')
+
+@section('title')
+    Home
+@endsection
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">
+	                <strong class="text-center">Welcome!</strong>
+	            </div>
+	            <div class="card-body">
+	            	PR75K Project
+	            </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
